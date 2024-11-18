cf116aa5f49a94e820d9ba5c231b22d750153beb
Bebs Fiorillo <estevaovrb2014@gmail.com>
2024-11-18 16:25:45 -0300

    define funcoes iniciais corretas Nó

--- No.cpp
+++ No.cpp
@@ -5,10 +5,30 @@ using namespace std;
 
 No::No()
 {
-
+    
 }
 
 No::~No()
 {
 
+}
+
+int No::getInfo()
+{
+    return info;
+}
+
+No* No::getProx()
+{
+    return prox;
+}
+
+void No::setInfo(int val)
+{
+    info = val;
+}
+
+void No::setProx(No* p)
+{
+    prox = p;   
 }
