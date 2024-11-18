b3474e24cb989abe271f7ad6ea72ea8f9ad87619
Bebs Fiorillo <estevaovrb2014@gmail.com>
2024-11-18 15:37:47 -0300

    configuração inicial

---
+++ SimplesEncadeada.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+#include "SimplesEncadeada.h"
+
+SimplesEncadeada::SimplesEncadeada()
+{
+
+}
+
+SimplesEncadeada::~SimplesEncadeada()
+{
+
+}
---
+++ SimplesEncadeada.h
@@ -0,0 +1,14 @@
+#ifndef SIMPLESENCADEADA_H__
+#define SIMPLESENCADEADA_H__
+#include <iostream>
+using namespace std;
+
+class SimplesEncadeada{
+    public:
+        SimplesEncadeada();
+        ~SimplesEncadeada();
+    private:
+
+};
+
+#endif
